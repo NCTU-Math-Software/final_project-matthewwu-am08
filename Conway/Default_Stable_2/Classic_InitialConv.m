ae88685b22c8f2409a9cb482f2d79f400ab85c79
matthewwu-am08 <71300187+matthewwu-am08@users.noreply.github.com>
2020-12-25 13:44:40 +0800

    Add files via upload

---
+++ Conway/Default_Stable_2/Classic_InitialConv.m
@@ -0,0 +1,111 @@
+%100m100
+%% Create initial.
+X = ones(100,100,3);
+X(1,2,1:3)=0;
+X(2,2,1:3)=0;
+X(3,2,1:3)=0;
+X(5,5,1:3)=0;
+X(6,4,1:3)=0;
+X(7,4,1:3)=0;
+X(8,5,1:3)=0;
+X(8,6,1:3)=0;
+X(6,6,1:3)=0;
+X(7,7,1:3)=0;
+X(10,10,1:3)=0;
+X(10,11,1:3)=0;
+X(10,12,1:3)=0;
+X(9,12,1:3)=0;
+X(8,11,1:3)=0;
+
+image(X);
+
+while true
+    [tx ty KEY] = ginput(1)
+    if(KEY==32)
+        break
+    end
+    X(round(ty),round(tx),1:3)=0;
+    image(X);drawnow;
+%loop.
+end
+while true
+    tic;
+    Y = X; %Snapshot X's condition.
+    for(ii=1:100)
+        for jj = 1:100
+            if(X(ii,jj,1)==0)
+                if(zpg(ii,jj,Y)<2)
+                    X(ii,jj,1:3)=1; %death.
+                end
+                if(zpg(ii,jj,Y)>3)
+                    X(ii,jj,1:3)=1; %death.
+                end
+            end
+            if(X(ii,jj,1)==1)
+                if(zpg(ii,jj,Y)==3)
+                    X(ii,jj,1:3)=0; %burn
+                end
+            end
+        end
+    end
+    image(X);
+    drawnow;
+    toc
+end
+%disp(zpg(1,100,X));
+
+
+function sum = zpg(a,b,X)
+    sum = 0;
+    try
+        k=X(a-1,b-1);
+    catch
+        k=1;
+    end
+    sum = sum+k;
+    try
+        k=X(a-1,b);
+    catch
+        k=1;
+    end
+    sum = sum+k;
+        try
+        k=X(a-1,b+1);
+    catch
+        k=1;
+    end
+    sum = sum+k;
+    
+       try
+        k=X(a,b+1);
+    catch
+        k=1;
+    end
+    sum = sum+k;
+    try
+        k=X(a,b-1);
+    catch
+        k=1;
+    end
+    sum = sum+k;
+    try
+        k=X(a+1,b-1);
+    catch
+        k=1;
+    end
+    sum = sum+k;
+    try
+        k=X(a+1,b);
+    catch
+        k=1;
+    end
+    sum = sum+k;
+    try
+        k=X(a+1,b+1);
+    catch
+        k=1;
+    end
+    sum = sum+k;
+    sum = 8 - sum;
+end
+
